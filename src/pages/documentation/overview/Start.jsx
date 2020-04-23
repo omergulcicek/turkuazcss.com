630c50404b6fe89c75ca56549cb157bc4f42d5a7
Ömer Gülçiçek <iletisim@omergulcicek.com>
2020-04-24 00:32:08 +0300

    Kurulum sayfası tamamlandı.

--- src/pages/documentation/overview/Start.jsx
+++ src/pages/documentation/overview/Start.jsx
@@ -1,4 +1,6 @@
 import React from "react"
+import { Link } from "react-router-dom"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 
 import { BasePage } from "../../../components"
 
@@ -37,7 +39,122 @@ export const obj = {
 
 const App = () => {
   return (
-    <BasePage info={obj} />
+    <>
+      <BasePage info={obj}>
+        <section>
+          <h2>CSS</h2>
+
+          <p>
+            CSS dosyasımızı diğer tüm stil dosyalarından önce <code>{`<head>`}</code>'e ekleyin.
+          </p>
+
+          <p>
+            <SyntaxHighlighter language="html">
+            {`<link rel="stylesheet" href="css/turkuaz.min.css">`}
+          </SyntaxHighlighter>
+          </p>
+
+          <p>
+            <a href="" className="button is-primary">İndir (v1.0.0)</a>
+            <a href="https://github.com/omergulcicek/turkuaz/releases" target="_blank" rel="noopener noreferrer" className="button is-secondary">Eski sürümler</a>
+          </p>
+        </section>
+        
+        <section>
+          <h2>Başlangıç Şablonu</h2>
+
+          <p>
+            HTML5 standartlarına göre hazırlanmış kodları kullanarak sayfanı hızlıca geliştirmeye başlayabilirsin.
+          </p>
+
+          <p>
+            <SyntaxHighlighter language="html">
+            {
+`<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Turkuaz</title>
+    <link rel="stylesheet" href="css/turkuaz.min.css">
+</head>
+<body>
+  <section>
+    <h1>Merhaba Dünya</h1>
+    <p>
+      Merhaba, bu benim <strong>Turkuaz</strong> ile ilk sitem!
+    </p>
+  </section>
+</body>
+</html>`
+            }
+            </SyntaxHighlighter>
+          </p>
+
+          <p>
+            <a href="" className="button is-primary is-outline">Başlangıç şablonunu indir</a>
+          </p>
+        </section>
+        
+        <section>
+          <h2>CDN</h2>
+
+          <p>
+            <SyntaxHighlighter>
+              {`https://cdnjs.cloudflare.com/ajax/libs/turkuaz/1.0.0/turkuaz.min.css`}
+            </SyntaxHighlighter>
+          </p>
+        </section>
+        
+        <section>
+          <h2>npm</h2>
+
+          <p>
+            Turkuaz'ı, <a href="https://www.npmjs.com/package/turkuaz" target="_blank" rel="noopener noreferrer">npm paketi</a> ile yükleyebilirsin.
+          </p>
+
+          <SyntaxHighlighter language="bash">
+            {`npm install turkuaz`}
+          </SyntaxHighlighter>
+        </section>
+
+        <section>
+          <h2>yarn</h2>
+
+          <p>
+            Turkuaz'ı, <a href="https://yarnpkg.com/tr/package/turkuaz" target="_blank" rel="noopener noreferrer">yarn paketi</a> ile yükleyebilirsin.
+          </p>
+
+          <p>
+            <SyntaxHighlighter language="bash">
+              {`yarn add turkuaz`}
+            </SyntaxHighlighter>
+          </p>
+        </section>
+
+        <section>
+          <h2>React</h2>
+
+          <p>
+            React projende Turkuaz'ı kullanmak istiyorsan, sadece CSS dosyasını sayfana dahil edebilir yada <strong>react-turkuaz</strong> paketini kullanabilirsin. 
+          </p>
+          <p>
+            Ayrıntılı bilgi için <Link to="/docs/library/react">React</Link> sayfasını inceleyin.
+          </p>
+        </section>
+
+        <section>
+          <h2>Vue</h2>
+
+          <p>
+            Vue projende Turkuaz'ı kullanmak istiyorsan, sadece CSS dosyasını sayfana dahil edebilir yada <strong>vue-turkuaz</strong> paketini kullanabilirsin. 
+          </p>
+          <p>
+            Ayrıntılı bilgi için <Link to="/docs/library/vue">Vue</Link> sayfasını inceleyin.
+          </p>
+        </section>
+      </BasePage>
+    </>
   )
 }
 
