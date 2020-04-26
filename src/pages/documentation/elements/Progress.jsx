d74e8484b3d268da2af2ca51eb52e1cf6c84e631
Ömer Gülçiçek <iletisim@omergulcicek.com>
2020-04-26 18:52:52 +0300

    Progress sayfası hazırlandı.

--- src/pages/documentation/elements/Progress.jsx
+++ src/pages/documentation/elements/Progress.jsx
@@ -26,7 +26,7 @@ export const obj = {
   ],
   pagination: {
     prev: {
-      href: "Overlay",
+      href: "overlay",
       text: "Overlay"
     },
     next: {
@@ -38,7 +38,77 @@ export const obj = {
 
 const App = () => {
   return (
-    <BasePage info={obj} />
+    <BasePage info={obj}>
+      <section>
+        <p>
+          HTML'in <code>&lt;progress></code> etiketine <code>progress</code> sınıfı ekleyerek bir Turkuaz ilerleme çubuğu oluşturabilirsin.
+        </p>
+
+        <div className="row mb-3">
+          <div className="col-12 col-md-6 col-lg-4">
+            <progress class="progress" value="15" max="100"></progress>
+          </div>
+        </div>
+
+        <SyntaxHighlighter language="html">
+{`<progress class="progress" value="15" max="100"></progress>`}
+        </SyntaxHighlighter>
+      </section>
+
+      <section>
+        <h2>Boyut</h2>
+
+        <p>
+          Progress elementi 3 farklı boyuta sahiptir.
+        </p>
+
+        <div className="row mb-3">
+          <div className="col-12 col-md-6 col-lg-4">
+            <progress class="progress is-small mb-1" value="15" max="100"></progress>
+            <progress class="progress mb-1" value="30" max="100"></progress>
+            <progress class="progress is-large" value="45" max="100"></progress>
+          </div>
+        </div>
+
+        <SyntaxHighlighter language="html">
+{`<progress class="progress is-small" value="15" max="100"></progress>
+
+<progress class="progress" value="30" max="100"></progress>
+
+<progress class="progress is-large" value="45" max="100"></progress>`}
+        </SyntaxHighlighter>
+      </section>
+
+      <section>
+        <h2>Renk</h2>
+
+        <p>
+          Progress elementi 5 farklı renge sahiptir.
+        </p>
+
+        <div className="row mb-3">
+          <div className="col-12 col-md-6 col-lg-4">
+            <progress class="progress mb-1" value="15" max="100"></progress>
+            <progress class="progress is-success mb-1" value="30" max="100"></progress>
+            <progress class="progress is-danger mb-1" value="45" max="100"></progress>
+            <progress class="progress is-warning mb-1" value="60" max="100"></progress>
+            <progress class="progress is-info mb-1" value="75" max="100"></progress>
+          </div>
+        </div>
+
+        <SyntaxHighlighter language="html">
+{`<progress class="progress" value="15" max="100"></progress>
+
+<progress class="progress is-success" value="30" max="100"></progress>
+
+<progress class="progress is-danger" value="45" max="100"></progress>
+
+<progress class="progress is-warning" value="60" max="100"></progress>
+
+<progress class="progress is-info" value="75" max="100"></progress>`}
+        </SyntaxHighlighter>
+      </section>
+    </BasePage>
   )
 }
 
