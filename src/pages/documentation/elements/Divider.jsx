43d4c915cafa8aef8ce4e01df2571cda613c5ba7
Ömer Gülçiçek <iletisim@omergulcicek.com>
2020-04-26 03:26:17 +0300

    Divider sayfası hazırlandı.

--- src/pages/documentation/elements/Divider.jsx
+++ src/pages/documentation/elements/Divider.jsx
@@ -38,7 +38,43 @@ export const obj = {
 
 const App = () => {
   return (
-    <BasePage info={obj} />
+    <BasePage info={obj}>
+      <section>
+        <p>
+          Divider elemanı sayfadaki içeriği dikey yada yatay olarak ikiye ayırır. <code>data-content</code> attribute ile ortasına küçük yazılar ekleyebilirsiniz.
+        </p>
+
+        <div className="row mb-3">
+          <div className="col-12 col-md-6 col-lg-4">
+            <div className="divider"></div>
+            <div className="divider" data-content="yada"></div>
+          </div>
+        </div>
+
+        <SyntaxHighlighter language="html">
+{`<div class="divider"></div>
+<div class="divider" data-content="yada"></div>
+`}
+        </SyntaxHighlighter>
+
+        <p className="mt-5">
+          Divider elementini dikey yapmak için <code>is-vertical</code> sınıfını eklemek yeterlidir.
+        </p>
+        
+        <div className="row">
+          <div className="col-12 col-md-6 col-lg-4">
+            <div className="divider is-vertical"></div>
+            <div className="divider is-vertical" data-content="yada"></div>
+          </div>
+        </div>
+
+        <SyntaxHighlighter language="html">
+{`<div class="divider is-vertical"></div>
+<div class="divider is-vertical" data-content="yada"></div>
+`}
+        </SyntaxHighlighter>
+      </section>
+    </BasePage>
   )
 }
 
