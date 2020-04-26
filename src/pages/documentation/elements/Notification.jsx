25e5e85dad21ccaa652d7eddc0a2be39e76b6ea8
Ömer Gülçiçek <iletisim@omergulcicek.com>
2020-04-26 03:26:35 +0300

    Notification sayfası hazırlandı.

--- src/pages/documentation/elements/Notification.jsx
+++ src/pages/documentation/elements/Notification.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 
 import { BasePage } from "../../../components"
@@ -38,7 +39,91 @@ export const obj = {
 
 const App = () => {
   return (
-    <BasePage info={obj} />
+    <BasePage info={obj}>
+      <section>
+        <p>
+          Notification, kullanıcıya bir şey hakkında dikkat çekmek isteyen basit renkli bir elementtir. <Link to="delete">Delete</Link> elementini destekler.
+        </p>
+
+        <div className="row mb-3">
+          <div className="col-12 col-md-6 col-lg-4">
+            <div className="notification">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              <span className="delete"></span>
+            </div>
+          </div>
+        </div>
+
+        <SyntaxHighlighter language="html">
+{`<div class="notification">
+  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+</div>
+`}
+        </SyntaxHighlighter>
+      </section>
+
+      <section>
+        <h2>Renk</h2>
+
+        <p>
+          Notification elementi 6 farklı renge sahiptir.
+        </p>
+
+
+        <div className="row mb-3">
+          <div className="col-12 col-md-6 col-lg-4">
+            <div className="notification">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6 col-lg-4">
+            <div className="notification is-primary">
+              <strong>is-primary</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6 col-lg-4">
+            <div className="notification is-success">
+              <strong>is-success</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </div>
+            
+          </div>
+          <div className="col-12 col-md-6 col-lg-4">
+            <div className="notification is-danger">
+              <strong>is-danger</strong>  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6 col-lg-4">
+            <div className="notification is-warning">
+              <strong>is-warning</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6 col-lg-4">
+            <div className="notification is-info">
+              <strong>is-info</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </div>
+          </div>
+        </div>
+
+<SyntaxHighlighter language="html">
+{`<div class="notification"></div>
+
+<div class="notification is-primary"></div>
+
+<div class="notification is-success"></div>
+
+<div class="notification is-danger"></div>
+
+<div class="notification is-warning"></div>
+
+<div class="notification is-info"></div>
+`}
+</SyntaxHighlighter>
+      </section>
+    </BasePage>
   )
 }
 
