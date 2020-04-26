8399532af77d266f8976ba81e0a790d95d63ccf6
Ömer Gülçiçek <iletisim@omergulcicek.com>
2020-04-26 18:53:10 +0300

    Title sayfası hazırlandı.

--- src/pages/documentation/elements/Title.jsx
+++ src/pages/documentation/elements/Title.jsx
@@ -38,7 +38,25 @@ export const obj = {
 
 const App = () => {
   return (
-    <BasePage info={obj} />
+    <BasePage info={obj}>
+      <section>
+        <p>
+          Normal başlıklara nazaran biraz daha büyük gösterişli başlık elementi. <code>&lt;h1>, &lt;h2>, &lt;h3>, &lt;h4>, &lt;h5>, &lt;h6></code> herhangi bir başlığa ilgili sınıfları ekleyebilirsin.
+        </p>
+
+        <h6 className="title is-size-1">Title 1</h6>
+        <h6 className="title is-size-2">Title 2</h6>
+        <h6 className="title is-size-3">Title 3</h6>
+        <h6 className="title is-size-4">Title 4</h6>
+
+        <SyntaxHighlighter language="html">
+{`<h6 class="title is-size-1">Title 1</h6>
+<h6 class="title is-size-2">Title 2</h6>
+<h6 class="title is-size-3">Title 3</h6>
+<h6 class="title is-size-4">Title 4</h6>`}
+        </SyntaxHighlighter>
+      </section>
+    </BasePage>
   )
 }
 
